5b50731a2cc787d16b4b2bee74ed56db5f0d0737
sortutay <s.ortutay@gmail.com>
2018-05-18 06:29:14 +0200

    code

---
+++ DiplomovaPraca/Poly.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomovaPraca
+{
+    class Poly
+    {
+        List<Unit> units;
+
+
+        public Poly()
+        {
+            units = new List<Unit>();
+        }
+
+        public Poly(List<Unit> units0)
+        {
+            units = units0;
+        }
+
+        public void Add(Unit u)
+        {
+            for (int i = 0; i<units.Count; i++)
+            {
+                Unit u2 = units[i].Add(u);
+                if (u2 != null)
+                {
+                    units[i] = u2;
+                    return;
+                }
+            }
+
+            units.Add(u);
+        }
+
+        public Poly Multiply(Poly p)
+        {
+            Poly result = new Poly();
+            for (int i = 0; i<units.Count; i++)
+            {
+                for (int j = 0; j<p.units.Count; j++)
+                {
+                    result.Add(units[i].Multiply(p.units[j]));
+                }
+            }
+
+            return result;
+        }
+
+        public Unit GetBase(int ex)
+        {
+            foreach(Unit u in units)
+            {
+                if (ex == u.Exponent)
+                {
+                    return u;
+                }
+            }
+
+            throw new ArgumentException($"Cant find variable with exponent {ex}");
+        }
+
+        public override string ToString()
+        {
+            var result = "Poly: ";
+            foreach(Unit u in units){
+                result += $"{u} ";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DiplomovaPraca/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomovaPraca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Unit x0 = new Unit("x", 1, 0);
+            Unit x1 = new Unit("x", 1, 1);
+            Unit x2 = new Unit("x", 1, 2);
+            Unit x3 = new Unit("x", 1, 3);
+            Unit x4 = new Unit("x", 1, 4);
+            Unit x5 = new Unit("x", 1, 5);
+            Unit x6 = new Unit("x", 1, 6);
+
+            
+
+
+            List < Unit> arr1 = new List<Unit>();
+            arr1.Add(x0);
+            arr1.Add(x1);
+            arr1.Add(x2);
+            arr1.Add(x3);
+            arr1.Add(x4);
+            arr1.Add(x5);
+            arr1.Add(x6);
+
+            List<Unit> arr2 = new List<Unit>();
+            arr2.Add(x0);
+            arr2.Add(x2);
+            arr2.Add(x4);
+            arr2.Add(x6);
+
+            List<Unit> arr3 = new List<Unit>();
+            arr3.Add(x0);
+            arr3.Add(x3);
+            arr3.Add(x6);
+
+            Poly p1 = new Poly(arr1);
+            Poly p2 = new Poly(arr2);
+            Poly p3 = new Poly(arr3);
+
+            Console.WriteLine(p1);
+
+            Poly p = p1.Multiply(p2);
+            Poly res = p.Multiply(p3);
+            Console.WriteLine(res.GetBase(20));
+
+            bool run = true;
+
+            while (run)
+            {
+                Console.WriteLine("Write your Poly -> ");
+                String poly = Console.ReadLine();
+                Console.WriteLine(poly);
+                Console.WriteLine("Do you want to exit? Y/N");
+                if ("y".Equals(Console.ReadLine())) run = false;
+                
+            }
+           
+
+
+        }
+    }
+}
